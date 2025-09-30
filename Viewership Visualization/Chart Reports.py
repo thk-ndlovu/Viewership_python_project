a0cbb12fdd2eb43b053dd33499ac60ffa9393a34
Kyle Ndlovu <tkndlovu111@gmail.com>
2025-09-30 14:45:03 +0200

    Delete Viewership Visualization directory

--- Viewership Visualization/Chart Reports.py
+++
@@ -1,8 +0,0 @@
-# Viewership Trend Over Time
-time_trend = df.groupby("DateID")["TotalTimeWatched"].sum()
-
-time_trend.plot(kind="line")
-plt.title("Viewership Trend Over Time")
-plt.ylabel("Total Time Watched")
-plt.xlabel("Date")
-plt.show()
